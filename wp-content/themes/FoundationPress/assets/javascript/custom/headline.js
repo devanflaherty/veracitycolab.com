d9a6043b8923063c9bd4ca24ca0eeee7a24263cd
Devan Flaherty <devanflaherty@me.com>
2016-06-16 00:04:52 -0600

    headline spacing fix for responsive

--- wp-content/themes/FoundationPress/assets/javascript/custom/headline.js
+++ wp-content/themes/FoundationPress/assets/javascript/custom/headline.js
@@ -1,1 +1,9 @@
-$('.headline').parent().css("padding-top","150px");
+$('.headline').parent().css("padding-top","120px");
+
+$(window).on('changed.zf.mediaquery', function(event, newSize, oldSize){
+  if (Foundation.MediaQuery.atLeast('medium')) {
+    $('.headline').parent().css("padding-top","120px");
+  } else {
+    $('.headline').parent().css("padding-top","200px");
+  }
+});
