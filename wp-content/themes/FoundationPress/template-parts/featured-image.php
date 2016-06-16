46460d323019da41691704467f43baa3dbc83439
Devan Flaherty <devanflaherty@me.com>
2016-06-15 23:38:09 -0600

    Featured Image fix
    Always have a featured section regardless if image is present or not.
    Only we will show primary color if there is no image.

--- wp-content/themes/FoundationPress/template-parts/featured-image.php
+++ wp-content/themes/FoundationPress/template-parts/featured-image.php
@@ -1,24 +1,30 @@
 <?php
-	// Got to figure out what colors to use!
-	if ( is_post_type_archive('work') ) {
-		$primaryColor = get_field( 'work_primary_color', 'option' );
-		$secondaryColor = get_field( 'work_secondary_color', 'option' );
-	} elseif ( is_post_type_archive('team') ) {
-		$primaryColor = get_field( 'team_primary_color', 'option' );
-		$secondaryColor = get_field( 'team_secondary_color', 'option' );
-	} else {
-		$primaryColor = substr(get_field( 'primary_color' ), 1);
-		$secondaryColor = substr(get_field( 'secondary_color' ), 1);
-	}
+if ( is_post_type_archive('work') ) {
+	$primaryColor = get_field( 'work_primary_color', 'option' );
+	$secondaryColor = get_field( 'work_secondary_color', 'option' );
+} elseif ( is_post_type_archive('team') ) {
+	$primaryColor = get_field( 'team_primary_color', 'option' );
+	$secondaryColor = get_field( 'team_secondary_color', 'option' );
+} elseif (get_field( 'primary_color' ) && get_field( 'secondary_color' )) {
+	$primaryColor = get_field( 'primary_color' );
+	$secondaryColor = get_field( 'secondary_color' );
+} else {
+	$primaryColor = get_field( 'global_primary_color', 'options' );
+	$secondaryColor = get_field( 'global_secondary_color', 'options' );
+}
 
 	// If a feature image is set, get the id, so it can be injected as a css background property
-	if ( has_post_thumbnail( $post->ID ) ) :
+	if ( has_post_thumbnail( $post->ID ) ) {
 		$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
 		$image = $image[0];
+		$style = "background-image: url('" . $image . "')";
+	} else {
+		$style = "background-color:" . $primaryColor . ";";
+	}
 ?>
 
 <!-- HERO!! DUH DUH DUUUUN -->
-<header id="featured-hero" role="banner" style="background-image: url('<?php echo $image ?>')">
+<header id="featured-hero" role="banner" style="<?php echo $style ?>">
 	<?php if ( get_field( 'video_id' ) && !empty(get_field( 'video_id' ))) :
 		// So if we can't find the "video_id" associated with the post, nothing after this comment will be seen. Sad.
 	?>
@@ -40,4 +46,3 @@
 	<?php endif; ?>
 </header>
 <!-- CLOSE HERO!! -->
-<?php endif;
