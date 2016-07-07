b913220057846d4bb72f068f828c1a15aa51fb40
Devan Flaherty <devanflaherty@me.com>
2016-07-07 11:22:25 -0600

    Removed Isotope

--- wp-content/themes/FoundationPress/assets/javascript/custom/isotope.js
+++ wp-content/themes/FoundationPress/assets/javascript/custom/isotope.js
@@ -1,7 +1,7 @@
-if ($("body").hasClass("post-type-archive-team")){
-// init Isotope
-
-// layout Isotope after each image loads
+// if ($("body").hasClass("post-type-archive-team")){
+// // init Isotope
+//
+// // layout Isotope after each image loads
 // var $grid = $('.grid');
 //   // init Isotope after all images have loaded
 // $grid.isotope({
@@ -9,26 +9,26 @@ if ($("body").hasClass("post-type-archive-team")){
 //   layoutMode: 'fitRows',
 //   percentPosition: true
 // });
-
-
-$(window).on('changed.zf.mediaquery', function(event, newSize, oldSize){
-  if(newSize ==  "small" || newSize ==  "mediun" || newSize ==  "large" ) {
-    $grid.isotope('layout');
-  }
-});
-
-// filter items on button click
-$('.filter-button-group').on( 'click', 'button', function() {
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
-});
-
-// change is-checked class on buttons
-$('.filter-button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
-});
-}
+//
+//
+// $(window).on('changed.zf.mediaquery', function(event, newSize, oldSize){
+//   if(newSize ==  "small" || newSize ==  "mediun" || newSize ==  "large" ) {
+//     $grid.isotope('layout');
+//   }
+// });
+//
+// // filter items on button click
+// $('.filter-button-group').on( 'click', 'button', function() {
+//   var filterValue = $(this).attr('data-filter');
+//   $grid.isotope({ filter: filterValue });
+// });
+//
+// // change is-checked class on buttons
+// $('.filter-button-group').each( function( i, buttonGroup ) {
+//   var $buttonGroup = $( buttonGroup );
+//   $buttonGroup.on( 'click', 'button', function() {
+//     $buttonGroup.find('.is-checked').removeClass('is-checked');
+//     $( this ).addClass('is-checked');
+//   });
+// });
+// }
