29efb8e9b5766f1549cf35f9ab5ee1b67ffa9f36
Devan Flaherty <devanflaherty@me.com>
2016-07-15 14:44:24 -0600

    Quickly UX and aspect fix

--- wp-content/themes/FoundationPress/template-parts/featured-image.php
+++ wp-content/themes/FoundationPress/template-parts/featured-image.php
@@ -48,15 +48,17 @@
 		  <div class="small-11 columns">
 				<div class="wistia_embed wistia_async_<?= $video; ?> videoFoam=true playerColor=<?= $secondaryColor; ?>" style="height:720px;width:1280px">&nbsp;</div>
 				<!-- WISTIA EMBED CODE BYE BYE -->
+		  </div>
+			<div class="small-11 columns">
 				<div class="row align-center" style="margin-top: 24px">
 					<div class="small-6 medium-3 columns">
-						<a class="button expanded radius secondary" href="#close">Close</a>
+						<a class="button expanded hollow upper white" href="#close">Close</a>
 					</div>
 					<div class="small-6 medium-3 columns">
-						<a class="button expanded radius secondary" id="seeProject" href="#">See Project</a>
+						<a class="button expanded hollow upper white" id="seeProject" href="#">See Project</a>
 					</div>
 				</div>
-		  </div>
+			</div>
 		</div>
 
 <?php endif; ?>
