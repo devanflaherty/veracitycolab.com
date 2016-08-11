38dc9aab24c665587afac383c11f89b42d2c442d
Devan Flaherty <devanflaherty@me.com>
2016-08-10 18:05:18 -0600

    Page Color Updates

--- wp-content/themes/FoundationPress/template-parts/featured-image.php
+++ wp-content/themes/FoundationPress/template-parts/featured-image.php
@@ -5,6 +5,9 @@
 	} elseif ( is_post_type_archive('team') ) {
 		$primaryColor = get_field( 'team_primary_color', 'option' );
 		$secondaryColor = get_field( 'team_secondary_color', 'option' );
+	} elseif ( is_post_type_archive('podcast') || is_singular('podcast') ) {
+		$primaryColor = get_field( 'podcast_primary_color', 'option' );
+		$secondaryColor = get_field( 'podcast_secondary_color', 'option' );
 	} elseif (get_field( 'primary_color' ) && get_field( 'secondary_color' )) {
 		$primaryColor = get_field( 'primary_color' );
 		$secondaryColor = get_field( 'secondary_color' );
