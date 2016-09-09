c2a56e8eca484a5929d4ae16b964511b5662c5b1
Devan Flaherty <devanflaherty@me.com>
2016-09-09 15:58:32 -0600

    Example Kit Migration

--- wp-content/themes/FoundationPress/template-parts/featured-image.php
+++ wp-content/themes/FoundationPress/template-parts/featured-image.php
@@ -22,7 +22,7 @@
 <!-- HERO!! DUH DUH DUUUUN -->
 <header id="featured-hero" role="banner" class="<?= $hidden; ?>" style="<?php echo $style ?>">
 <?php
-	if ( is_post_type_archive( 'work' ) || is_post_type_archive( 'podcast' ) || is_singular( 'work' ) || is_singular( 'advance' )) :
+	if ( is_post_type_archive( 'work' ) || is_post_type_archive( 'podcast' ) || is_singular( 'work' ) || is_singular( 'example' )) :
 		if(get_field('video_id')) {
 			$video = get_field('video_id');
 		} else {
@@ -30,7 +30,7 @@
 		}
 	?>
 
-	<?php if(!is_singular( 'advance' ) && !empty(get_field('video_id'))) : ?>
+	<?php if(!is_singular( 'example' ) && !empty(get_field('video_id'))) : ?>
 		<div class="feature-play"><i class="fa fa-play" aria-hidden="true"></i></div>
 	<?php endif; ?>
 
@@ -46,7 +46,7 @@
 					<div class="small-6 medium-3 columns">
 						<a class="button expanded hollow upper white" href="#close">Close</a>
 					</div>
-					<?php if(is_post_type_archive( 'work' ) || is_singular( 'advance' )) : ?>
+					<?php if(is_post_type_archive( 'work' ) || is_singular( 'example' )) : ?>
 						<div class="small-6 medium-3 columns">
 							<a class="button expanded hollow upper white" id="seeProject" href="#">See Project</a>
 						</div>
