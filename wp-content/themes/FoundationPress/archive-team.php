e5560a8f739cc40c8b8adb173ce204ac79e445a5
Devan Flaherty <devanflaherty@me.com>
2016-09-23 13:13:37 -0600

    Pagination Styling

--- wp-content/themes/FoundationPress/archive-team.php
+++ wp-content/themes/FoundationPress/archive-team.php
@@ -94,13 +94,13 @@ get_header(); ?>
 		<?php endif; // End have_posts() check. ?>
 
 	</article>
-	<?php /* Display navigation to next/previous pages when applicable */ ?>
-	<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
-		<nav id="post-nav">
-			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-		</nav>
-	<?php } ?>
 </section>
+<?php /* Display navigation to next/previous pages when applicable */ ?>
+<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
+	<nav id="post-nav">
+		<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
+		<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
+	</nav>
+<?php } ?>
 
 <?php get_footer();
