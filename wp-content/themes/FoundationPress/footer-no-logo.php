1f4b2f21874f9688a2e4e6db132e352b019b4525
Devan Flaherty <devanflaherty@me.com>
2016-10-13 17:00:04 -0600

    Remove Adroll

--- wp-content/themes/FoundationPress/footer-no-logo.php
+++ wp-content/themes/FoundationPress/footer-no-logo.php
@@ -56,7 +56,6 @@
 </div><!-- Close off-canvas content wrapper -->
 <?php endif; ?>
 
-
 <?php wp_footer(); ?>
 <?php do_action( 'foundationpress_before_closing_body' ); ?>
 </div><!-- CLOSE SKROLLR -->
@@ -90,28 +89,5 @@ _hsq.push(["setContentType", "listing-page"]);
 </script>
 <!-- End of Async HubSpot Analytics Code -->
 <script src="//fast.wistia.com/static/integrations-hubspot-v1.js" async></script>
-
-<!-- ADROLL -->
-<script type="text/javascript">
-    adroll_adv_id = "54AQ6GAS5VBXLHNLKPR5AN";
-    adroll_pix_id = "ZMNSTLLRVZG4JLTSEBDDYQ";
-    /* OPTIONAL: provide email to improve user identification */
-    /* adroll_email = "username@example.com"; */
-    (function () {
-        var _onload = function(){
-            if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
-            if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
-            var scr = document.createElement("script");
-            var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
-            scr.setAttribute('async', 'true');
-            scr.type = "text/javascript";
-            scr.src = host + "/j/roundtrip.js";
-            ((document.getElementsByTagName('head') || [null])[0] ||
-                document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
-        };
-        if (window.addEventListener) {window.addEventListener('load', _onload, false);}
-        else {window.attachEvent('onload', _onload)}
-    }());
-</script>
 </body>
 </html>
